bcf682e121d3587c96989ebe768a06657ca62ffe
Mattias Villani <mattias.villani@gmail.com>
2017-08-30 21:16:09 +0200

    Ny tenta med lösningar. Fixade pmf figur i F2.

--- Figures/pmf_cdf.m
+++ Figures/pmf_cdf.m
@@ -4,9 +4,9 @@ set(0,'DefaultAxesFontSize',16)
 x = [-1 0 1 2 3 4 5];
 y = [nan 0.15 0.2 0.4 0.2 0.05 nan];
 
-h = bar(x, y, 'BarWidth',0.01, 'edgecolor','b');
+h = bar(x(2:end), y(2:end), 'BarWidth',0.01, 'edgecolor','b');
 hold
-h = plot(x, y, 'b.','markersize',30);
+h = plot(x(2:end), y(2:end), 'b.','markersize',30);
 box off
 ylabel('P(x)')
 xlabel('x')
@@ -28,17 +28,17 @@ title('cdf','fontsize',18)
 print cdf -dpng
 
 
-figure('name','pmf')
-set(0,'DefaultAxesFontSize',16)
-
-x = [- 1 0 1 2 3 4 5];
-y = [nan 0 0.25 0.7 0.05 0 nan];
-
-h = bar(x, y, 'BarWidth',0.01, 'edgecolor','b');
-hold
-h = plot(x, y, 'b.','markersize',30);
-box off
-ylabel('P(x)')
-xlabel('x')
-title('pmf','fontsize',18)
-print pmf -dpng
+% figure('name','pmf')
+% set(0,'DefaultAxesFontSize',16)
+% 
+% x = [- 1 0 1 2 3 4 5];
+% y = [nan 0 0.25 0.7 0.05 0 nan];
+% 
+% h = bar(x, y, 'BarWidth',0.01, 'edgecolor','b');
+% hold
+% h = plot(x, y, 'b.','markersize',30);
+% box off
+% ylabel('P(x)')
+% xlabel('x')
+% title('pmf','fontsize',18)
+% print pmf -dpng
